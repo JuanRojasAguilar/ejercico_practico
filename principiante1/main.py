c40d5915b350d95d4d72dc49543e3a98a03af175
Juan Rojas <juanrojas@Juans-MacBook-Pro.local>
2024-02-17 10:40:14 -0500

    principiante 1 done

--- principiante1/main.py
+++ principiante1/main.py
@@ -1,5 +1,22 @@
-import modulos.menus as menus
+lista = []
+def preguntar_numero():
+  numero = int(input("Ingrese un número entero positivo: "))
+  if numero > 0:
+    lista.append(numero)
+  else:
+    print("Por favor ingresa un número válido.")
+    preguntar_numero()
+
+
 if __name__ == "__main__":
   def main():
-    menus.menu_principal()
+    contador = 0
+    for _ in range(3):
+        preguntar_numero()
+    for numero in lista:
+        contador += numero
+    print(f"La suma de tus números son {contador}")
+    
+
+
   main()
--- principiante1/modulos/reportes.py
+++
@@ -1,9 +0,0 @@
-from . import estudiantes
-
-def total_peso(clasificacion):
-  contador = 0
-  for estudiante in estudiantes.listado_estudiante:
-    if estudiante["clasificacion"] == clasificacion:
-      contador += 1
-
-  print(f"Los estudiantes con un con clasificación {clasificacion} son {contador}")
